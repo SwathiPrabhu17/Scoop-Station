80597904943a665f982d495402ce552def4bbc4e
SwathiPrabhu17 <swathiprabhu075@gmail.com>
2025-04-10 17:14:17 +0530

    Updated styles and added footer copyright

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-// Toggle search bar
 document.addEventListener("DOMContentLoaded", () => {
-    const searchToggle = document.getElementById("search-toggle");
-    const searchBar = document.getElementById("search-bar");
-  
-    searchToggle.addEventListener("click", () => {
-      if (searchBar.style.display === "block") {
-        searchBar.style.display = "none";
-      } else {
-        searchBar.style.display = "block";
-      }
-    });
+  const searchToggle = document.getElementById("search-toggle");
+  const searchBar = document.getElementById("search-bar");
+
+  searchToggle.addEventListener("click", () => {
+    searchBar.classList.toggle("visible");
   });
+});
